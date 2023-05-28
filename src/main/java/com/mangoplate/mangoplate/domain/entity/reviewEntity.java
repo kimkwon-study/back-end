59354e586222596b760270dc60f2e2c8b782337c
kmg159753 <mingyugim584@gmail.com>
2023-05-28 17:09:38 +0900

    Update reviewEntity.java

--- src/main/java/com/mangoplate/mangoplate/domain/entity/reviewEntity.java
+++ src/main/java/com/mangoplate/mangoplate/domain/entity/reviewEntity.java
@@ -1,4 +1,16 @@
 package com.mangoplate.mangoplate.domain.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="review",  uniqueConstraints = {@UniqueConstraint(columnNames = "post_id")})
+
 public class reviewEntity {
 }
