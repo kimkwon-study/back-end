3f56dc143d8081550cd911c1d9de0d4e06d0f53e
kmg159753 <mingyugim584@gmail.com>
2023-05-28 22:50:49 +0900

    Create PostRepository.java

---
+++ src/main/java/com/mangoplate/mangoplate/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package com.mangoplate.mangoplate.repository;
+
+import com.mangoplate.mangoplate.domain.entity.PostEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<PostEntity,Long> {
+}
