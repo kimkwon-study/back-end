35407d0de11c5ec33bc6b76ca0ba7aedbe89ea91
kmg159753 <mingyugim584@gmail.com>
2023-05-28 18:10:36 +0900

    Update PostEntity.java

--- src/main/java/com/mangoplate/mangoplate/domain/entity/PostEntity.java
+++ src/main/java/com/mangoplate/mangoplate/domain/entity/PostEntity.java
@@ -16,7 +16,7 @@ public class PostEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "post_id", unique = true, nullable = false)
-    private String postId;
+    private Long postId;
 
     @Column
     private String restaurantName;
@@ -58,7 +58,7 @@ public class PostEntity {
 
     private PostEntity(){}
 
-    private PostEntity(String postId,String restaurantName,String restaurantAddress,String phoneNum,
+    private PostEntity(Long postId,String restaurantName,String restaurantAddress,String phoneNum,
                        String foodCategory,String price,Boolean parking,String businessTime,String breakTime,
                        String breakDay,String websiteUrl,String menu){
 
@@ -77,14 +77,24 @@ public class PostEntity {
 
     }
 
-    public static PostEntity getEntity(String postId,String restaurantName,String restaurantAddress,String phoneNum,
+    public static PostEntity getEntity(Long postId,String restaurantName,String restaurantAddress,String phoneNum,
                        String foodCategory,String price,Boolean parking,String businessTime,String breakTime,
                        String breakDay,String websiteUrl,String menu){
 
         return new PostEntity(postId,restaurantName,restaurantAddress,phoneNum,
-                foodCategory,price, parking,businessTime,breakTime, breakDay,websiteUrl,menu)
+                foodCategory,price, parking,businessTime,breakTime, breakDay,websiteUrl,menu);
 
     }
+
+//    public int getReviewCount() {
+//        // 데이터베이스에서 리뷰 개수를 조회하는 쿼리 실행
+//        // 예를 들어, JPA의 EntityManager를 사용하여 쿼리를 실행하는 방법:
+//        EntityManager entityManager = ...; // EntityManager 생성
+//        Query query = entityManager.createQuery("SELECT COUNT(r) FROM reviewEntity r WHERE r.postEntity = :post");
+//        query.setParameter("post", this);
+//        Long count = (Long) query.getSingleResult();
+//        return count.intValue();
+//    }
 
 
 }
