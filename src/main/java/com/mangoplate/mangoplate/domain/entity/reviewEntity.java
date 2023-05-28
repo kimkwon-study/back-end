c26037ced08b890d32362dd88aa659953ec88ae5
kmg159753 <mingyugim584@gmail.com>
2023-05-28 17:19:27 +0900

    리뷰 Entity 추가

--- src/main/java/com/mangoplate/mangoplate/domain/entity/reviewEntity.java
+++ src/main/java/com/mangoplate/mangoplate/domain/entity/reviewEntity.java
@@ -1,11 +1,13 @@
 package com.mangoplate.mangoplate.domain.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Date;
+import java.sql.Timestamp;
 
 @Getter
 @Setter
@@ -13,4 +15,36 @@ import lombok.Setter;
 @Table(name="review",  uniqueConstraints = {@UniqueConstraint(columnNames = "post_id")})
 
 public class reviewEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id", unique = true, nullable = false)
+    private String reviewId;
+
+    @Column
+    @CreationTimestamp
+    private Date uploadAt;
+
+    @Column
+    private String content;
+
+    @Column
+    private String imageUrl;
+
+
+
+    private  reviewEntity(){}
+
+    private  reviewEntity(String reviewId, Date uploadAt, String content, String imageUrl){
+        this.reviewId = reviewId;
+        this.uploadAt = uploadAt;
+        this.content = content;
+        this.imageUrl = imageUrl;
+    }
+
+    public static reviewEntity getEntity(String reviewId, Date uploadAt, String content, String imageUrl){
+        return new reviewEntity(reviewId, uploadAt, content, imageUrl);
+    }
+
+
 }
