814a021bd350ff1288c2fc2b892eb995546a0b2f
kmg159753 <mingyugim584@gmail.com>
2023-05-28 22:50:54 +0900

    Create PostService.java

---
+++ src/main/java/com/mangoplate/mangoplate/service/PostService.java
@@ -0,0 +1,38 @@
+package com.mangoplate.mangoplate.service;
+
+
+import com.mangoplate.mangoplate.domain.entity.PostEntity;
+import com.mangoplate.mangoplate.domain.request.PostRequest;
+import com.mangoplate.mangoplate.domain.response.PostResponse;
+import com.mangoplate.mangoplate.domain.type.ErrorCode;
+import com.mangoplate.mangoplate.exception.ApplicationException;
+import com.mangoplate.mangoplate.repository.PostRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class PostService {
+
+
+    private final PostRepository postRepository;
+
+
+    public PostRequest get_post(PostRequest request) {
+
+        PostEntity res = postRepository.findById(request.postId()).orElseThrow(() -> {
+            throw new ApplicationException(ErrorCode.NO_POST);
+        });
+
+        PostRequest postRequest = request;
+
+        return postRequest;
+    }
+
+}
