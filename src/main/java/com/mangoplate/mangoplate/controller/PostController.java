1ba60180aa6c5d54a4079a651b817328a8e71072
kmg159753 <mingyugim584@gmail.com>
2023-05-28 22:50:07 +0900

    Create PostController.java

---
+++ src/main/java/com/mangoplate/mangoplate/controller/PostController.java
@@ -0,0 +1,49 @@
+package com.mangoplate.mangoplate.controller;
+
+import com.mangoplate.mangoplate.domain.request.PostRequest;
+import com.mangoplate.mangoplate.domain.request.UserJoinRequest;
+import com.mangoplate.mangoplate.domain.request.UserLoginRequest;
+import com.mangoplate.mangoplate.domain.response.PostResponse;
+import com.mangoplate.mangoplate.domain.response.Response;
+import com.mangoplate.mangoplate.domain.response.UserRegisterResponse;
+import com.mangoplate.mangoplate.service.PostService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api")
+public class PostController {
+
+    private final PostService postService;
+
+    @GetMapping("/post")
+    public Response<PostRequest> getPost(@RequestBody PostRequest request) {
+        PostRequest postRequest = postService.get_post(request);
+        return Response.success(postRequest);
+    }
+
+    @PostMapping("/post")
+    public Response<PostResponse> createPost(@RequestBody PostRequest request) {
+        PostResponse postResponse = userService.post_write(request);
+        return Response.success(postResponse);
+    }
+
+
+//    @GetMapping("/review")
+//    public Response<ReviewResponse> getPost(@RequestBody ReviewRequest request){
+//        ReviewResponse reviewResponse = userService.get_review(request.getReviewId);
+//        return Response.success(postResponse);
+//    }
+
+
+
+//    @PostMapping("/review")
+//    public Response<ReviewResponse> createReview(@RequestBody ReviewRequest request){
+//        ReviewResponse reviewResponse = userService.review_write(request);
+//        return Response.success(reviewResponse);
+//    }
+
+
+
+}
