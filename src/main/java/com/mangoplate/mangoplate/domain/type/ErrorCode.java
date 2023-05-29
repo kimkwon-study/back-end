cd73e014f08b83ffacab592add8e117724456e44
kmg159753 <mingyugim584@gmail.com>
2023-05-29 17:40:26 +0900

    리뷰 없을 때 에러코드

--- src/main/java/com/mangoplate/mangoplate/domain/type/ErrorCode.java
+++ src/main/java/com/mangoplate/mangoplate/domain/type/ErrorCode.java
@@ -14,7 +14,8 @@ public enum ErrorCode {
     LOGIN_NO_JOIN(HttpStatus.UNAUTHORIZED,"가입되지 않은 아이디입니다."),
     //회원가입
     REGISTER_SAME_ID(HttpStatus.UNAUTHORIZED,"동일한 아이디가 존재합니다."),
-    NO_POST(HttpStatus.UNAUTHORIZED,"게시글이 없습니다.")
+    NO_POST(HttpStatus.UNAUTHORIZED,"게시글이 없습니다."),
+    NO_REVIEW(HttpStatus.UNAUTHORIZED,"리뷰가 없습니다.")
     ;
 
     private final HttpStatus status;
