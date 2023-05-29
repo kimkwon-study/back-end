da4d05904b662d614c55e958d4cf1477a16c5882
kmg159753 <mingyugim584@gmail.com>
2023-05-29 17:39:54 +0900

    contorller 리뷰 조회

---
+++ src/main/java/com/mangoplate/mangoplate/controller/ReviewController.java
@@ -0,0 +1,47 @@
+package com.mangoplate.mangoplate.controller;
+
+import com.mangoplate.mangoplate.domain.request.ReviewRequest;
+import com.mangoplate.mangoplate.domain.response.Response;
+import com.mangoplate.mangoplate.domain.response.ReviewResponse;
+import com.mangoplate.mangoplate.service.ReviewService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api")
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @GetMapping("/review")
+    public Response<ReviewResponse> getReview(@RequestBody ReviewRequest request){
+        ReviewResponse reviewResponse = reviewService.get_review(request);
+        return Response.success(reviewResponse);
+    }
+
+//    @PostMapping("/post")
+//    public Response<PostResponse> createPost(@RequestBody PostRequest request) {
+//        postService.write_post(request);
+//
+//        return Response.success(null);
+//
+//    }
+//
+//    @DeleteMapping("/post")
+//    public void deletePost(@RequestBody PostRequest request) {
+//        postService.delete_post(request);
+//    }
+//
+//
+//
+//
+//
+////    @PostMapping("/review")
+////    public Response<ReviewResponse> createReview(@RequestBody ReviewRequest request){
+////        ReviewResponse reviewResponse = userService.review_write(request);
+////        return Response.success(reviewResponse);
+////    }
+
+
+}
