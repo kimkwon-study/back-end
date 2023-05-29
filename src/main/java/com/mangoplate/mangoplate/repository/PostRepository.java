a2187fcad47639418d3af23f4fe55973351803eb
kmg159753 <mingyugim584@gmail.com>
2023-05-29 18:57:32 +0900

    Update PostRepository.java

--- src/main/java/com/mangoplate/mangoplate/repository/PostRepository.java
+++ src/main/java/com/mangoplate/mangoplate/repository/PostRepository.java
@@ -1,7 +1,13 @@
 package com.mangoplate.mangoplate.repository;
 
 import com.mangoplate.mangoplate.domain.entity.Post;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+import java.util.Optional;
+
+
 public interface PostRepository extends JpaRepository<Post,Long> {
+    Optional<Post> findByRestaurantName(String name);
 }
