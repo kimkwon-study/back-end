32a427c72c16dc46dbb85b76b7003c5a35895a9e
kmg159753 <mingyugim584@gmail.com>
2023-05-31 02:46:33 +0900

    Update ReviewResponse.java

--- src/main/java/com/mangoplate/mangoplate/domain/response/ReviewResponse.java
+++ src/main/java/com/mangoplate/mangoplate/domain/response/ReviewResponse.java
@@ -1,14 +1,17 @@
 package com.mangoplate.mangoplate.domain.response;
 
 import com.mangoplate.mangoplate.domain.entity.Post;
+import com.mangoplate.mangoplate.domain.type.TasteRating;
 import org.hibernate.tool.schema.internal.SchemaTruncatorImpl;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 public record ReviewResponse(
         String reviewCode,
         String content,
-        String imageUrl,
+        List<String> imageUrl,
+        TasteRating tasteRating,
         Post post,
         Timestamp registeredAt,
         Timestamp updatedAt
