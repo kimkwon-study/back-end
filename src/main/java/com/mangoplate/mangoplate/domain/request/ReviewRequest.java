c4521370fcfc6a5b001fb76be0a3551d519cb477
kmg159753 <mingyugim584@gmail.com>
2023-05-31 02:46:42 +0900

    Update ReviewRequest.java

--- src/main/java/com/mangoplate/mangoplate/domain/request/ReviewRequest.java
+++ src/main/java/com/mangoplate/mangoplate/domain/request/ReviewRequest.java
@@ -1,14 +1,17 @@
 package com.mangoplate.mangoplate.domain.request;
 
 import com.mangoplate.mangoplate.domain.entity.Post;
+import com.mangoplate.mangoplate.domain.type.TasteRating;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 public record ReviewRequest(
         Long reviewId,
         String reviewCode,
         String content,
-        String imageUrl,
+        List<String> imageUrl,
+        TasteRating taste,
         Post post,
         Timestamp registeredAt,
         Timestamp updatedAt
