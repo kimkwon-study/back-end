5ae041063725b80ce18a34c5fd422f653591ca1b
kmg159753 <mingyugim584@gmail.com>
2023-05-31 02:15:14 +0900

    Update PostRepository.java

--- src/main/java/com/mangoplate/mangoplate/repository/PostRepository.java
+++ src/main/java/com/mangoplate/mangoplate/repository/PostRepository.java
@@ -1,7 +1,7 @@
 package com.mangoplate.mangoplate.repository;
 
 import com.mangoplate.mangoplate.domain.entity.Post;
-import org.springframework.data.domain.Example;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
