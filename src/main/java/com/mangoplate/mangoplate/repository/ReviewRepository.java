c0b4e8f7fb27058c28742bd3c369c438f637e3f1
kmg159753 <mingyugim584@gmail.com>
2023-05-31 17:34:12 +0900

    Update ReviewRepository.java

--- src/main/java/com/mangoplate/mangoplate/repository/ReviewRepository.java
+++ src/main/java/com/mangoplate/mangoplate/repository/ReviewRepository.java
@@ -4,11 +4,12 @@ package com.mangoplate.mangoplate.repository;
 import com.mangoplate.mangoplate.domain.entity.Review;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 
-public interface ReviewRepository extends JpaRepository<Review,Long> {
+public interface ReviewRepository extends JpaRepository <Review,Long> {
 
     Optional<Review> findByReviewCode(String reviewCode);
 }
